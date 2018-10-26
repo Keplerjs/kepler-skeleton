06e4ad44ecdd3fb1adaacc2d78edce5a1a78a70b
Stefano Cudini <stefano.cudini@gmail.com>
2018-10-26 14:56:27 +0200

    Update heroku.sh

--- private/heroku.sh
+++ private/heroku.sh
@@ -3,3 +3,5 @@
 #this script deploy keplerjs app in heroku servers
 
 #https://medium.com/@leonardykris/how-to-run-a-meteor-js-application-on-heroku-in-10-steps-7aceb12de234
+
+#and a mongodb database in mlab.com
